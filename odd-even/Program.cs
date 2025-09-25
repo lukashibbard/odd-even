d9cc03278f4fb2a23f6b7e2110bb6dd02a9e4b62
lukashibbard <lh0999775@otc.edu>
2025-09-25 08:35:25 -0500

    Add files via upload

---
+++ odd-even/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("please give me a number ill tell you if its odd or even");
+int num = Convert.ToInt32(Console.ReadLine());
+int check = num % 2;
+if (check == 1)
+{
+    Console.WriteLine("odd");
+}
+else
+{
+    Console.WriteLine("even");
+}
